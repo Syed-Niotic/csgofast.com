1de4d5755b2249ccf853169f2817bd85c1998f97
xiongzhenyu <xzy.xiongzhenyu@gmail.com>
2016-03-07 17:58:32 +0800

    fix files

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
+var ejs = require('ejs')
 var path = require('path')
 var express = require('express')
 var port = process.env.PORT || 3000
+
 var app = express()
 
-app.set('views', './views/pages')
-app.set('view engine', 'jade')
+// app.set('views', './views/pages')
+app.set('views', './static')
+// app.set('view engine', 'jade')
+app.engine('html', ejs.renderFile)
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'bower_components')))
 app.listen(port)
 
-console.log('listening on *: ' + port + ' ')
+console.log('Express server listening on port *: ' + port + ' ')
 
 // Route_index
 app.get('/', function(req, res) {
-  res.render('index', {
-    title: 'CSGOFAST.COM - TRY YOUR LUCK!'
-  })
+    res.render('index.html', {
+        title: 'CSGOFAST.COM - TRY YOUR LUCK!'
+    })
 })
