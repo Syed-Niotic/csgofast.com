40a3f0ac5a1e8fb05baff9f36ad8e89993750f4b
xiongzhenyu <xzy.xiongzhenyu@gmail.com>
2016-03-22 17:47:44 +0800

    fix some files

---
+++ schemas/user.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose')
+
+var UserSchema = new mongoose.Schema({
+    name: String,
+    steamID: String,
+    email: String,
+    phone: Number,
+    tradeUrl: String
+})
+
+UserSchema.pre('add', function(next) {
+    if (this.isNew) {
+
+    }
+
+    next()
+})
+
+UserSchema.static = {
+    fetch: function(cb) {
+        return this
+            .find({})
+            .sort('')
+        exec(cb)
+    },
+    findById: function(id, cb) {
+        return this
+            .findOne({
+                _id: id
+            })
+        exec(cb)
+    }
+}
+
+module.exprots = UserSchema
